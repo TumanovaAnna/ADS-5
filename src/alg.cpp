d6976ab2a9c1f7e2d87a8c809e6c3aea10a0adb6
TumanovaAnna <118038015+TumanovaAnna@users.noreply.github.com>
2023-04-06 21:00:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,117 +2,115 @@
 #include <string>
 #include <map>
 #include "tstack.h"
-
 int getPrior(char op) {
-  std::pair<char, int> prior1[6];
+  std::pair<char, int> priority[6];
   switch (op) {
     case'(':
-      prior1[0].first = '(';
-      prior1[0].second = 0;
+      priority[0].first = '(';
+      priority[0].second = 0;
     case')':
-      prior1[1].first = ')';
-      prior1[1].second = 1;
+      priority[1].first = ')';
+      priority[1].second = 1;
     case'+':
-      prior1[2].first = '+';
-      prior1[2].second = 2;
+      priority[2].first = '+';
+      priority[2].second = 2;
     case'-':
-      prior1[3].first = '-';
-      prior1[3].second = 2;
+      priority[3].first = '-';
+      priority[3].second = 2;
     case'*':
-      prior1[4].first = '*';
-      prior1[4].second = 3;
+      priority[4].first = '*';
+      priority[4].second = 3;
     case'/':
-      prior1[5].first = '/';
-      prior1[5].second = 3;
+      priority[5].first = '/';
+      priority[5].second = 3;
   }
   int prior = -1;
   for (int j = 0; j < 6; ++j) {
-    if (op == prior1[j].first) {
-      prior = prior1[j].second;
+    if (op == priority[j].first) {
+      prior = priority[j].second;
       break;
     }
   }
   return prior;
 }
-std::string spc(const std::string& s) {
+std::string miracle1(const std::string& s) {
   if (s.length() <= 2) return s;
   int n = 2 - s.length() % 2;
   std::string r(s, 0, n);
-  for (auto i = s.begin() + n; i != s.end();) {
-    r += ' '; r += *i++;;
+  for (auto it = s.begin() + n; it != s.end();) {
+    r += ' '; r += *it++;;
   }
   return r;
 }
 std::string infx2pstfx(std::string inf) {
   std::string work;
-  TStack <char, 100> stack;
+  Tstack<char, 100> stack1;
   for (auto& op : inf) {
     int prior = getPrior(op);
     if (prior == -1) {
       work += op;
     } else {
-      if (stack.get() < prior || prior == 0 || stack.isEmpty()) {
-        stack.push(op);
+      if (stack1.get() < prior || prior == 0 || stack1.isEmpty()) {
+        stack1.push(op);
       } else if (op == ')') {
-        char a = stack.get();
-        while (getPrior(a) >= prior) {
-          work += a;
-          stack.pop();
-          a = stack.get();
+        char sm = stack1.get();
+        while (getPrior(sm) >= prior) {
+          work += sm;
+          stack1.pop();
+          sm = stack1.get();
         }
-        stack.pop();
+        stack1.pop();
       } else {
-        char a = stack.get();
-        while (getPrior(a) >= prior) {
-          work += a;
-          stack.pop();
-          a = stack.get();
+        char sm = stack1.get();
+        while (getPrior(sm) >= prior) {
+          work += sm;
+          stack1.pop();
+          sm = stack1.get();
         }
-        stack.push(op);
+        stack1.push(op);
       }
     }
   }
-  while (!stack.isEmpty()) {
-    work += stack.get();
-    stack.pop();
+  while (!stack1.isEmpty()) {
+    work += stack1.get();
+    stack1.pop();
   }
-  work = spc(work);
+  work = miracle1(work);
   return work;
 }
-
-int count(const int& n1, const int& n2, const int& oper) {
+int count(const int& a, const int& b, const int& oper) {
   switch (oper) {
     default:
       break;
-    case'+': return n1 + n2;
-    case'-': return n1 - n2;
-    case'*': return n1 * n2;
-    case'/': return n1 / n2;
+    case'+': return a + b;
+    case'-': return a - b;
+    case'*': return a * b;
+    case'/': return a / b;
   }
   return 0;
 }
 int eval(std::string pref) {
-  TStack<int, 100> stack;
+  Tstack<int, 100> stack1;
   std::string num = "";
   for (size_t i = 0; i < pref.size(); i++) {
     if (getPrior(pref[i]) == -1) {
       if (pref[i] == ' ') {
         continue;
-      } else if (isdigit(pref[i+1])) {
+      } else if (isdigit(pref[i + 1])) {
         num += pref[i];
         continue;
       } else {
         num += pref[i];
-        stack.push(atoi(num.c_str()));
+        stack1.push(atoi(num.c_str()));
         num = "";
       }
     } else {
-      int n2 = stack.get();
-      stack.pop();
-      int n1 = stack.get();
-      stack.pop();
-      stack.push(count(n1, n2, pref[i]));
+      int b = stack1.get();
+      stack1.pop();
+      int a = stack1.get();
+      stack1.pop();
+      stack1.push(count(a, b, pref[i]));
     }
   }
-  return stack.get();
+  return stack1.get();
 }
